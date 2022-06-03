580882bc0f3ee18bd35d407bc954dc5e72cad834
Lucca Hellriegel <lucca.hellriegel@gmx.net>
2022-06-03 20:59:10 +0200

    init spring

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # PirateManagement
+
+An app for pirates to manage their treasure hunts. Arrrr!
+A collection of best practices I want to remember and a playground to experiment.
---
+++ service/src/main/java/com/arrr/piratery/PirateryApplication.java
@@ -0,0 +1,13 @@
+package com.arrr.piratery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PirateryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PirateryApplication.class, args);
+	}
+
+}
