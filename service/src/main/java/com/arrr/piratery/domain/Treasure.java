ee63d88d160ccd85ce66505cf2dfa47737eaddbf
Lucca Hellriegel <lucca.hellriegel@gmx.net>
2022-06-04 23:11:30 +0200

    added querydsl

--- service/src/main/java/com/arrr/piratery/domain/Treasure.java
+++ service/src/main/java/com/arrr/piratery/domain/Treasure.java
@@ -7,9 +7,11 @@ import javax.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
 
 @Data
 @AllArgsConstructor
+@Document
 public class Treasure implements PersistenceObject {
 
   @Id
